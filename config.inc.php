fb33efff9fa2b7834c881a34ce547d1e06059d42
Sam Nilrazy <76619946+samnilrazy@users.noreply.github.com>
2022-01-01 16:37:50 -0400

    Add files via upload

--- config.inc.php
+++ config.inc.php
@@ -3,7 +3,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie
  */
-$cfg['Servers'][$i]['Host']="https://phpmyadmin2.herokuapp.com/"; /* YOU SHOULD CHANGE THIS FOR A MORE SECURE COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'xampp'; /* YOU SHOULD CHANGE THIS FOR A MORE SECURE COOKIE AUTH! */
 
 /*
  * Servers configuration
@@ -17,14 +17,14 @@ $i++;
 
 /* Authentication type and info */
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'localhost';
+$cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 $cfg['Lang'] = '';
 
 /* Bind to the localhost ipv4 address and tcp */
-$cfg['Servers'][$i]['host'] = 'https://phpmyadmin2.herokuapp.com';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 
 /* User for advanced features */
