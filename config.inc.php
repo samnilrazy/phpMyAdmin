ba0b7a780f2e5b8ecc104337a954f9b8b643770e
Sam Nilrazy <76619946+samnilrazy@users.noreply.github.com>
2022-01-01 16:29:02 -0400

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -17,7 +17,7 @@ $i++;
 
 /* Authentication type and info */
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['user'] = 'localhost';
 $cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
